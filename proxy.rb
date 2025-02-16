4357165e120c5da53c1a4cb41faea9b3e23a0f38
Vito Botta <vito@botta.me>
2025-02-16 15:29:28 +0200

    Handle cases where upstream request body might be empty

--- proxy.rb
+++ proxy.rb
@@ -57,8 +57,12 @@ class CompletionsProxy < Sinatra::Base
 
     http.read_timeout = 3600
 
-    request_body = JSON.parse(upstream_request.body)
-    is_streaming = request_body['stream'] == true
+    is_streaming = if upstream_request.body
+      request_body = JSON.parse(upstream_request.body)
+      is_streaming = request_body['stream'] == true
+    else
+      false
+    end
 
     if is_streaming
       headers 'Content-Type' => 'text/event-stream'
